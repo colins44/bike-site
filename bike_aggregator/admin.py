06b3bbf602d75d86e08199b47726dc17c9681200
Colin Pringle-Wood <colin.pringlewood@gmail.com>
2015-09-29 09:00:38 +0100

    change bike search admin

--- bike_aggregator/admin.py
+++ bike_aggregator/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import BikeSearch, BikeShop
 
+class BikeSearchAdmin(admin.ModelAdmin):
+    list_display = ['location', 'bike_type', 'no_of_bikes']
+
+
 admin.site.register(BikeShop)
-admin.site.register(BikeSearch)
+admin.site.register(BikeSearch, BikeSearchAdmin)
