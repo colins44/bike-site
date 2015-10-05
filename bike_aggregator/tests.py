45619e5dbdb1c3fcf3502885640032f4c41f93e7
Colin Pringle-Wood <colin.pringlewood@gmail.com>
2015-10-05 09:43:35 +0100

    merge ab testing

--- bike_aggregator/tests.py
+++ bike_aggregator/tests.py
@@ -7,7 +7,7 @@ from .forms import BikeRentalForm, SignUpForm
 class TestForms(TestCase):
 
     def test_get_home_page(self):
-        resp = self.client.get('/')
+        resp = self.client.get('/bikes-to-rent/')
         self.assertEqual(resp.status_code, 200)
 
     def test_bike_search_form(self):
@@ -21,29 +21,29 @@ class TestForms(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_bike_shop_form(self):
-        signup_form_data={
+        signup_form_data = {
             'town_or_region': 'paris',
             'country': 'France',
             'shop_name': 'road_bike',
             'website': 'https://google.com',
-            'email':'email@example.com'
+            'email': 'admin@example.com'
         }
         form = SignUpForm(data=signup_form_data)
         self.assertTrue(form.is_valid())
 
     def test_post_to_home_page(self):
         bike_rental_form_data = {
-            'location': 'paris',
+            'location': 'harare',
             'bike_type': 'road_bike',
             'no_of_bikes': 10,
-            'email':'email@example.com'
+            'email': 'admin@example.com'
         }
         resp = self.client.post(
-            path='/',
+            path='/bicycles-to-rent/',
             data=bike_rental_form_data
         )
         self.assertEqual(resp.status_code, 302)
-        bike_search = BikeSearch.objects.get(email=bike_rental_form_data['email'])
+        bike_search = BikeSearch.objects.get(location=bike_rental_form_data['location'])
         bike_search = model_to_dict(bike_search)
         self.assertDictContainsSubset(bike_rental_form_data, bike_search)
         self.assertEqual(BikeSearch.objects.all().count(), 1)
