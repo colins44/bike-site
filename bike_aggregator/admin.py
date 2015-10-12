fd499d0d657ef726624cd9fd63daadbd5367e959
Colin Pringle-Wood <colin.pringlewood@gmail.com>
2015-10-12 14:16:46 +0100

    adding RentalEquipment to admin

--- bike_aggregator/admin.py
+++ bike_aggregator/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import BikeSearch, BikeShop
+from .models import BikeSearch, BikeShop, RentalEquipment
+
 
 class BikeSearchAdmin(admin.ModelAdmin):
     list_display = ['location', 'bike_type', 'no_of_bikes']
 
 
 admin.site.register(BikeShop)
+admin.site.register(RentalEquipment)
 admin.site.register(BikeSearch, BikeSearchAdmin)
