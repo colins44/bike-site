2205977e9ebb5910e6a50222340054af6b9ca8c7
Colin Pringlewood <colin.pringlewood@gmail.com>
2015-10-30 16:59:40 +0000

    changing mail provider

--- my_project/settings/prod_settings.py
+++ my_project/settings/prod_settings.py
@@ -2,7 +2,7 @@ from my_project.settings.settings import *
 
 # DEBUG = False
 ALLOWED_HOSTS = ['localhost']
-DEBUG = True
+DEBUG = False
 
 #THIS IS WHERE ALL THE STATIC FILES ARE COPIED TO WHEN COLLECT STATIC IS RUN
 #NGINX SOULD POINT TO THIS FILE TO FIND THE STATIC FILES
@@ -13,11 +13,10 @@ EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'this_is_not_the_password')
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'enquiry@youvelo.com'
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = EMAIL_ADDRESS'
 EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
 DEFAULT_FROM_EMAIL = EMAIL_ADDRESS
 DEFAULT_TO_EMAIL = EMAIL_ADDRESS
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
