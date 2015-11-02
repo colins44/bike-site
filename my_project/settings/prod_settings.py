14f65b0a904384e5e137ad3b39867b7d6dd8eb10
Colin Pringlewood <colin.pringlewood@gmail.com>
2015-11-02 08:52:09 +0000

    remove other email methods

--- my_project/settings/prod_settings.py
+++ my_project/settings/prod_settings.py
@@ -8,16 +8,12 @@ DEBUG = True
 #NGINX SOULD POINT TO THIS FILE TO FIND THE STATIC FILES
 STATIC_ROOT = os.path.join(os.getcwd(), "static")
 
-EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS', 'enquiry@youvelo.com')
-EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'this_is_not_the_password')
-
-
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = EMAIL_ADDRESS
-EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
-DEFAULT_FROM_EMAIL = EMAIL_ADDRESS
-DEFAULT_TO_EMAIL = EMAIL_ADDRESS
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS', 'enquiry@youvelo.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'this_is_not_the_password')
+DEFAULT_FROM_EMAIL = 'enquiry@youvelo.com'
+DEFAULT_TO_EMAIL = 'enquiry@youvelo.com'
 EMAIL_FROM_ADDR = 'enquiry@youvelo.com'
