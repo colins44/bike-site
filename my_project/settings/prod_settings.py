0a4b42b75c5dd500ca2e8c48234b6310bfae04a1
Colin Pringlewood <colin.pringlewood@gmail.com>
2015-11-02 12:44:13 +0000

    adding user

--- my_project/settings/prod_settings.py
+++ my_project/settings/prod_settings.py
@@ -1,5 +1,4 @@
 from my_project.settings.settings import *
-from auth import EMAIL_ADDRESS, EMAIL_PASSWORD
 
 # DEBUG = False
 ALLOWED_HOSTS = ['*']
@@ -13,8 +12,8 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = EMAIL_ADDRESS
-EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS", 'testing')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", 'One_two')
 DEFAULT_FROM_EMAIL = 'enquiry@youvelo.com'
 DEFAULT_TO_EMAIL = 'enquiry@youvelo.com'
 EMAIL_FROM_ADDR = 'enquiry@youvelo.com'
