3a56b91901df30d60843208443a25c2ed17c4ff6
Colin Pringlewood <colin.pringlewood@gmail.com>
2015-12-01 09:08:04 +0000

    fixing sitemaps

--- bike_aggregator/sitemaps.py
+++ bike_aggregator/sitemaps.py
@@ -9,7 +9,12 @@ class StaticSiteMap(Sitemap):
     def items(self):
         #get a list of city names with no duplicates
         cities = BikeShop.objects.all().values_list('city', flat=True).distinct()
-        city_urls = ["/bike-shop-search-results/{}/".format(city.lower()) for city in cities]
+        city_urls = []
+        for city in cities:
+            try:
+                city_urls.append("/bike-shop-search-results/{}/".format(city.lower()))
+            except:
+                pass
         city_urls.append("/about/")
         city_urls.append("/sign-up/")
         city_urls.append("/contact/")
