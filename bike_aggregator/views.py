0eb6467d9099b60053e8094260b65292ca9c887a
colins44 <colin.pringlewood@gmail.com>
2016-01-06 21:41:27 +0000

    adding events

--- bike_aggregator/views.py
+++ bike_aggregator/views.py
@@ -33,7 +33,6 @@ class Index(FormView):
 
     def form_valid(self, form):
         super(Index, self).form_valid(form)
-        import ipdb; ipdb.set_trace()
         self.request.session.__setitem__('bike_search', form.cleaned_data)
         if form.cleaned_data['latitude'] and form.cleaned_data['longitude']:
             return redirect('bike-shop-search-results',
@@ -64,11 +63,9 @@ class BikeSearchResults(ListView):
     def get(self, request, *args, **kwargs):
 
         if request.session.get('visited', False):
-            print 'visted'
             pass
         else:
             request.session['visited'] = True
-            print 'visited false so set to true'
             message = 'You can find bike shops that rent out the type of bikes you are looking for with the filter button'
             messages.add_message(request, messages.INFO, message)
         return super(BikeSearchResults, self).get(request, *args, **kwargs)
@@ -230,58 +227,24 @@ class ShopCreateView(CrudMixin, CreateView):
         return HttpResponseRedirect('/profile/')
 
 
-# def bikeshopview(request, pk):
-#
-#     bikeshop = get_object_or_404(BikeShop, pk=pk)
-#     if request.method == "POST":
-#
-#         form = BookingListAdd(request.post)
-#
-#         if form.is_valid():
-#             message = 'shop added to booking request list'
-#             request.messages.add_message(request, request.messages.info, message)
-#             request.session.__setitem__('booking_request_list', pk=pk)
-#         else:
-#             message = 'something went wrong adding the bike shop to the booking request list'
-#             request.messages.add_message(request, request.messages.warning, message)
-#
-#         return render(request, 'shop_detail_page.html', {'form': form, 'bikeshop': bikeshop, 'messages': messages})
-#
-#     if request.method == 'GET':
-#         if request.session.get('visited', False):
-#             print 'visited'
-#             request.session.flush()
-#             messages = None
-#         else:
-#             request.session['visited'] = True
-#             print 'not visited, where is the message'
-#             message = 'Send a booking request to this shop by filling out the Booking Request Form or send multipul booking request by clicking "add to request list" button and send your booking request to many stores at once'
-#             request.messages.add_message(request, request.messages.INFO, message)
-#
-#     form = BookingListAdd()
-#     return render(request, 'shop_detail_page.html', {'form': form, 'bikeshop': bikeshop, 'messages': messages})
-
-
-
-
 class BikeShopView(FormView):
     model = BikeShop
     template_name = 'shop_detail_page.html'
     form_class = ReservationRequestForm
 
     def get(self, request, *args, **kwargs):
-        if request.session.get('visited', False):
-            print 'visited'
-            request.session.flush()
+        if self.request.session.get('visited', False):
+            self.request.session.flush()
+            pass
         else:
-            request.session['visited'] = True
+            self.request.session['visited'] = True
             print 'not visited, where is the message'
             message = 'Send a booking request to this shop by filling ' \
                       'out the Booking Request Form or send multipul booking ' \
                       'request by clicking "add to request list" button ' \
                       'and send your booking request to many stores at once'
-            messages.add_message(request, messages.INFO, message)
-        return super(BikeShopView, self).get(request, *args, **kwargs)
+            messages.add_message(self.request, messages.INFO, message)
+        return super(BikeShopView, self).get(self.request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(BikeShopView, self).get_context_data(**kwargs)
@@ -300,6 +263,17 @@ class BikeShopView(FormView):
             self.request.session['booking_message'] = True
             message = "Reservation Request sent to {}".format(bikeshop.shop_name)
         messages.add_message(self.request, messages.INFO, message)
+        context = {}
+        context['bikeshop'] = bikeshop
+        context['form_data'] = form.cleaned_data
+        email = EMail(to=bikeshop.email, subject='Bike Hire Enquiry')
+        email.text('emails/reservations_request.txt', context)
+        email.html('emails/reservations_request.html', context)
+        email.send()
+        Event.objects.create(
+            name='Reservations Request email sent',
+            data="Reservation Request sent from: {} to: {}".format(bikeshop.email, form.cleaned_data.get('email'))
+        )
         return super(BikeShopView, self).form_valid(form)
 
 
